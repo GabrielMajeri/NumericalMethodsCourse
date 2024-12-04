6f016956bd3f39405a3543f79269399d6e50ee19
Gabriel Majeri <gabriel.majeri6@gmail.com>
2024-12-04 14:03:05 +0200

    Implement ascending Gauss-Seidel method

--- src/NumericalMethodsCourse.jl
+++ src/NumericalMethodsCourse.jl
@@ -2,7 +2,8 @@ module NumericalMethodsCourse
 
 using LinearAlgebra
 
-export StoppingCriterion, correction, residual, jacobi
+export StoppingCriterion, correction, residual,
+    jacobi, gauss_seidel
 
 @enum StoppingCriterion correction residual
 
@@ -23,7 +24,7 @@ function check_inputs_size(A::AbstractMatrix{T}, b::AbstractVector{T},
     end
 end
 
-"Compute the inverse of a given matrix using the Jacobi iteration method."
+"Computes the solution of a linear system using the Jacobi iteration method."
 function jacobi(
     A::AbstractMatrix{T}, b::AbstractVector{T},
     x⁰::AbstractVector{T}, max_iterations::Int,
@@ -35,9 +36,6 @@ function jacobi(
     D = Diagonal(diag(A))
     # Compute its inverse
     D_inverse = inv(D)
-
-    # println("D = ", D)
-    # println("D^{-1} = ", D_inverse)
 
     # Create a counter variable for the number of iterations
     # required for the algorithm to converge
@@ -54,17 +52,18 @@ function jacobi(
 
     # While the stopping criterion is not met
     while true
-        # println(num_iterations)
-
         # Apply the Jacobi iteration step,
         #     x_{n + 1} = (I - D^{-1} A) * x_{n} + D^{-1} b
         new_solution = iteration_matrix * solution + additive_term
 
-        # println(new_solution)
-
         # Update the error criteria
         corr = new_solution - solution
         res = b - A * new_solution
+
+        # Update the solution vector
+        solution = new_solution
+        # Increment the number of iterations
+        num_iterations += 1
 
         # Compute the error vector
         error = similar(b)
@@ -76,13 +75,7 @@ function jacobi(
             error("Invalid stopping criterion")
         end
 
-        # Update the solution vector
-        solution = new_solution
-        # Increment the number of iterations
-        num_iterations += 1
-
         # Check if we've converged
-        # println(norm(error))
         if norm(error) <= tolerance
             break
         end
@@ -95,6 +88,77 @@ function jacobi(
     end
 
     # Return the solution the algorithm converged to and the number of iterations it took to reach it
+    solution, num_iterations
+end
+
+"Computes the solution of a linear system using the (ascending) Gauss-Seidel method."
+function gauss_seidel(
+    A::AbstractMatrix{T}, b::AbstractVector{T},
+    x⁰::AbstractVector{T}, max_iterations::Int,
+    tolerance::Float64, criterion::StoppingCriterion) where {T <: Number}
+
+    check_inputs_size(A, b, x⁰)
+
+    n = length(b)
+
+    # Initialize the solution variable with the initial guess
+    solution = x⁰
+
+    # Create a buffer vector for the new solution vector being constructed
+    new_solution = similar(x⁰)
+
+    # Create a counter variable for the number of iterations
+    # required for the algorithm to converge
+    num_iterations = 0
+
+    # While the stopping criterion is not yet met
+    while true
+        # Compute the new solution vector, component-by-component
+        for i = 1:n
+            δ₁ = 0
+            for j = 1:i-1
+                δ₁ += A[i, j] * new_solution[j]
+            end
+
+            δ₂ = 0
+            for j = i+1:n
+                δ₂ += A[i, j] * solution[j]
+            end
+
+            new_solution[i] = (1 / A[i, i]) * (b[i] - δ₁ - δ₂)
+        end
+
+        # Update the error criteria
+        corr = new_solution - solution
+        res = b - A * new_solution
+
+        # Update the solution vector (by making a swap to conserve memory)
+        solution, new_solution = new_solution, solution
+        # Increment the number of iterations
+        num_iterations += 1
+
+        # Compute the error vector
+        error = similar(b)
+        if criterion == correction
+            error = corr
+        elseif criterion == residual
+            error = res
+        else
+            error("Invalid stopping criterion")
+        end
+
+        # Check if we're converged
+        if norm(error) <= tolerance
+            break
+        end
+
+        # Stop if we've reached the maximum number of iterations
+        # we've been permitted to use
+        if num_iterations >= max_iterations
+            break
+        end
+    end
+
     solution, num_iterations
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
 using NumericalMethodsCourse
 using Test
 
-const A1 = [3 5 3; 2 2 3]
-const b1 = [1; 3]
+const A1 = [3. 5. 3.; 2. 2. 3.]
+const b1 = [1.; 3.]
 
-const A5 = [3 -1 0; -1 3 -1; 0 -1 3]
-const b5 = [2; 1; 2]
+const A5 = [3. -1. 0.; -1. 3. -1.; 0. -1. 3.]
+const b5 = [2.; 1.; 2.]
 
 const max_iterations = 1_00
 const tolerance = 1e-14
@@ -26,9 +26,17 @@ function check_convergence(x_out, num_iters_out, x_expected, num_iters_expected)
 end
 
 @testset "Jacobi" begin
-    @test check_error(_ -> jacobi(A1, b1, [0; 0], max_iterations, tolerance, correction))
+    @test check_error(_ -> jacobi(A1, b1, [0.; 0.], max_iterations, tolerance, correction))
     @test check_convergence(
-        jacobi(A5, b5, [0; 0; 0], max_iterations, tolerance, correction)...,
+        jacobi(A5, b5, [0.; 0.; 0.], max_iterations, tolerance, correction)...,
         A5 \ b5, 44
+    )
+end
+
+@testset "Gauss-Seidel ascending" begin
+    @test check_error(_ -> gauss_seidel(A1, b1, [0.; 0.], max_iterations, tolerance, correction))
+    @test check_convergence(
+        gauss_seidel(A5, b5, [0.1; 0.2; 0.3], max_iterations, tolerance, correction)...,
+        A5 \ b5, 23
     )
 end
